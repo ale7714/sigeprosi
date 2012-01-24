f5e9e6c344a27756ab55002cd43d6c380cb1bea6
fernandosahmkow1@hotmail.com <fernandosahmkow1@hotmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-01-24 18:44:56 +0000

    Version inicial del Encriptador.

---
+++ class/Encrypter.php
@@ -0,0 +1,37 @@
+ <?php
+	/*	UNIVERSIDAD SIMON BOLIVAR
+		PERIODO:			ENE-MAR 2012
+		MATERIA: 			SISTEMAS DE INFORMACION II
+		NOMBRE DEL ARCHIVO: Encrypter.php
+	*/
+
+class Encrypter {
+		
+        private $clave;
+        private $id;
+		
+		/*	Parametros de entrada:
+		Parametros de salida: 
+					Objeto del tipo usuario
+		Descripcion	: Constructor de la clase usuario.					
+		*/
+   		function __construct($clave,$id=0) {
+			$this->clave    = $clave;
+			$this->id       = $id;
+        }
+			
+		
+	   	/*  Parametros de entrada:
+						NINGUNO
+		Parametros de salida: 
+					cadena encriptada unidireccionalmente
+		Descripcion	: Función que permite encriptar usando MD5		
+		*/
+        public function toMD5() {
+			CRYPT_MD5 = 1;
+            $result = crypt($clave, $id);
+			return $result;
+	   	}
+		
+}
+?>
