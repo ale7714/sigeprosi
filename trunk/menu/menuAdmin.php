f2132eca327e1fcd313e93a50168982bed55a3d4
javierrodriguez9@gmail.com <javierrodriguez9@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-06 17:05:47 +0000

    menuAdmin

--- trunk/menu/menuAdmin.php
+++ trunk/menu/menuAdmin.php
@@ -3,11 +3,19 @@
 	<div id="templatemo_menu">
     	<ul>
             <li><a href="?content=inicio" <?if ($page=='inicio'){?>class="current fast"<?}?>><b>Inicio</b></a></li>
-            <li><a href="?content=avances" <?if ($page=='avances'){?>class="current fast"<?}?>><b>Avances</b></a></li>
-            <li><a href="?content=proyectos" <?if ($page=='proyectos'){?>class="current fast"<?}?>><b>Proyectos</b></a></li>
-            <li><a href="?content=evaluaciones" <?if ($page=='evaluaciones'){?>class="current fast"<?}?>><b>Evaluaciones</b></a></li>
-            <li><a href="?content=consultar" <?if ($page=='consultar'){?>class="current fast"<?}?>><b>Consultar</b></a></li>
-			<li><a href="?content=registroUsuario" <?if ($page=='registrar'){?>class="current fast"<?}?>><b>Registrar</b></a></li>
+            <li><a href="#" <?if ($page=='avances'){?>class="current fast"<?}?>><b>Avances</b></a></li>
+            <li><a href="#" <?if ($page=='proyectos'){?>class="current fast"<?}?>><b>Proyectos</b></a></li>
+            <li><a href="#" <?if ($page=='evaluaciones'){?>class="current fast"<?}?>><b>Evaluaciones</b></a></li>
+            <li><a href="#" <?if ($page=='consultar'){?>class="current fast"<?}?>><b>Consultar</b></a></li>
+			<li><a href="#" <?if ($page=='respaldos'){?>class="current fast"<?}?>><b>Respaldos</b></a></li>
+			<li><a href="?content=registrar" <?if ($page=='registrar'){?>class="current fast"<?}?>><b>Registrar</b></a></li>
+
+            <!-- li><a href="?content=avances" < ?if ($page=='avances'){?>class="current fast"< ?}?>><b>Avances</b></a></li>
+            <li><a href="?content=proyectos" < ?if ($page=='proyectos'){?>class="current fast"< ?}?>><b>Proyectos</b></a></li>
+            <li><a href="?content=evaluaciones" < ?if ($page=='evaluaciones'){?>class="current fast"< ?}?>><b>Evaluaciones</b></a></li>
+            <li><a href="?content=consultar" < ?if ($page=='consultar'){?>class="current fast"< ?}?>><b>Consultar</b></a></li>
+			<li><a href="?content=registroUsuario" < ?if ($page=='registrar'){?>class="current fast"< ?}?>><b>Registrar</b></a></li -->
+			
         </ul>
     </div> <!-- end of menu -->
     
