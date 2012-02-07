7b973c0573d88981c0e0e5c2d582095d4df247aa
duque.cristian@gmail.com <duque.cristian@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-07 03:06:06 +0000

    acomode la interfaz de cambiar contraseña, ha problemas con el tamaño del menu de administrador, las opciones no caben

--- trunk/menu/menuAdmin.php
+++ trunk/menu/menuAdmin.php
@@ -3,8 +3,13 @@
 	<div id="templatemo_menu">
     	<ul>
             <li><a href="?content=inicio" <?if ($page=='inicio'){?>class="current fast"<?}?>><b>Inicio</b></a></li>
+
+            <li><a href="#" <?if ($page=='avances'){?>class="current fast"<?}?>><b>Avances</b></a></li>
+            <li><a href="#" <?if ($page=='agregarProyecto'){?>class="current fast"<?}?>><b>Proyectos</b></a></li>
+
             <li><a href="#" <?if ($page=='equipos'){?>class="current fast"<?}?>><b>Equipos</b></a></li>
             <li><a href="?content=agregarProyecto" <?if ($page=='proyectos'){?>class="current fast"<?}?>><b>Proyectos</b></a></li>
+
             <li><a href="#" <?if ($page=='evaluaciones'){?>class="current fast"<?}?>><b>Evaluaciones</b></a></li>
             <li><a href="#" <?if ($page=='etapas'){?>class="current fast"<?}?>><b>Etapas</b></a></li>
 			<li><a href="?content=solicitudes" <?if ($page=='solicitudes'){?>class="current fast"<?}?>><b>Solicitudes</b></a></li>
