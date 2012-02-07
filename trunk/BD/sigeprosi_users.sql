1472bd91593f98f89dd6b5709906560ce25b99db
fernandosahmkow1@hotmail.com <fernandosahmkow1@hotmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-07 00:10:28 +0000

    Agregada seguridad de clave a usuarios, leer el LEEMEX.txt

---
+++ trunk/BD/sigeprosi_users.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `correoUSB`, `password`, `correoOpcional`, `activo`) VALUES
+(2, 'Fernando', 'Sahmkow', '08-11027@usb.ve', 'f3b10516780afa89840cff7ef718be4b', '', 1),
+(3, 'Administrador', 'sigeprosi', 'admin@usb.ve', 'a65f68b5d67e8cbf97d30aeec673fef6', '', 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- trunk/acciones/cambiarContrasena.php
+++ trunk/acciones/cambiarContrasena.php
@@ -1,14 +1,20 @@
 <?php
-include_once "../class/class.Usuario.php";
+$root = $_SERVER['DOCUMENT_ROOT']."/sigeprosi/";
+include_once $root."/class/class.Usuario.php";
+include_once $root."/snippets/generarSal.php";
+include_once $root."/class/class.Encrypter.php";
 session_start();
-if (isset($_SESSION["correoUSB"])){
-	$u = new Usuario(null,null,$_SESSION["correoUSB"],$_POST["pass"],null,null);
-    if ($u->autocompletar() != 0 || $u->get('password') != $_POST["pass"])
+if (isset($_SESSION["correoUSB"])) {
+    $enc = new Encrypter($_POST["pass"], generarSal($_SESSION["correoUSB"]));
+    $codigo = $enc->toMD5();
+	$u = new Usuario(null,null,$_SESSION["correoUSB"],$codigo,null,null);
+    if ($u->autocompletar() != 0 || $u->get('password') != $codigo)
         echo "No encontrado";
     else {
-        $u->set("password",$_POST["passnew"]);
+        $enc = new Encrypter($_POST["passnew"], generarSal($_SESSION["correoUSB"]));
+        $u->set("password",$enc->toMD5());
         $u->actualizar($_SESSION["correoUSB"]);
-        //header("Location: ../principal.php?content=cambiarContrasenaExito");
+        header("Location: ../principal.php?content=cambiarContrasenaExito");
     }
 }
 else
