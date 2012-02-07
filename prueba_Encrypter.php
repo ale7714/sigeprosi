be71f91d5b42fe75b7d22d8ba8d11b71a6f6c41e
fernandosahmkow1@hotmail.com <fernandosahmkow1@hotmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-07 00:35:11 +0000

    borre las pruebas no necesarias.

--- prueba_Encrypter.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once $_SERVER['DOCUMENT_ROOT']."/sigeprosi/"."/class/class.Encrypter.php";
-    include_once $_SERVER['DOCUMENT_ROOT']."/sigeprosi/"."/snippets/redirect.php";
-	$enc = new Encrypter("sho sho", 1);
-    echo $enc->toMD5();
-?>
