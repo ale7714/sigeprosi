4cfdddf7449aec3a3b803156ca8fa28602a97681
fernandosahmkow1@hotmail.com <fernandosahmkow1@hotmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-12 20:18:56 +0000

    Ultimos commit

--- trunk/menu/menuAdmin.php
+++ trunk/menu/menuAdmin.php
@@ -5,7 +5,7 @@
             <li><a href="?content=inicio" <?if ($page=='inicio'){?>class="current fast"<?}?>><b>Inicio</b></a></li>
             <!--<li><a href="#" <?if ($page=='avances'){?>class="current fast"<?}?>><b>Avances</b></a></li> -->
             <!--li><a href="#" <?if ($page=='agregarProyecto'){?>class="current fast"<?}?>><b>Proyectos</b></a></li-->
-            <li><a href="#" <?if ($page=='equipos'){?>class="current fast"<?}?>><b>Equipos</b></a></li>
+            <!--<li><a href="#" <?if ($page=='equipos'){?>class="current fast"<?}?>><b>Equipos</b></a></li>-->
             <li><a href="?content=proyectos" <?if ($page=='proyectos'){?>class="current fast"<?}?>><b>Proyectos</b></a></li>
             <li><a href="#" <?if ($page=='evaluaciones'){?>class="current fast"<?}?>><b>Evaluaciones</b></a></li>
             <li><a href="?content=etapas" <?if ($page=='etapas'){?>class="current fast"<?}?>><b>Planificaciones</b></a></li>
