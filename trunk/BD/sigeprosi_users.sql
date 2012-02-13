c70b31bd4181749ecdda4a3a102d004d1c3cc557
javierrodriguez9@gmail.com <javierrodriguez9@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-13 19:31:14 +0000

    Usuarios con carnet válido

--- trunk/BD/sigeprosi_users.sql
+++ trunk/BD/sigeprosi_users.sql
@@ -1,9 +1,9 @@
 INSERT INTO `usuario` (`nombre`, `apellido`, `correoUSB`, `password`, `correoOpcional`, `activo`, `rol`, `carnetOCedula`) VALUES
 ('Fernando', 'Sahmkow', '08-11027@usb.ve', 'f3b10516780afa89840cff7ef718be4b', '', 1, 0, 08-11027),
 ('Administrador', 'sigeprosi', 'admin@usb.ve', 'a65f68b5d67e8cbf97d30aeec673fef6', '', 1, 0, 00-00000),
-('Victor', 'Rivero', '04-37494@usb.ve', 'faa77f5e39f651db2ffa4ce764004db1', '', 1, 0, '04-37494@u'),
-('Ivanhoe ', 'Gamarra', '05-38194@usb.ve', '6cfcd7d8d5e31f97ea37a94e3933cc6f', '', 1, 0, '05-38194@u'),
-('Alexandra', 'Paredes', '05-38680@usb.ve', 'e9be238502e6e51725f6cdcdb6c4458a', '', 1, 0, '05-38680@u'),
-('Eric', 'Mendillo', '06-39902@usb.ve', '2d42b211e479cf44a08600924d17cf73', '', 1, 0, '06-39902@u'),
-('Anselmo', 'Munoz', '07-41260@usb.ve', 'b61a27ee1b107f3afa064ff051799858', '', 1, 0, '07-41260@u');
+('Victor', 'Rivero', '04-37494@usb.ve', 'faa77f5e39f651db2ffa4ce764004db1', '', 1, 0, '04-37494'),
+('Ivanhoe ', 'Gamarra', '05-38194@usb.ve', '6cfcd7d8d5e31f97ea37a94e3933cc6f', '', 1, 0, '05-38194'),
+('Alexandra', 'Paredes', '05-38680@usb.ve', 'e9be238502e6e51725f6cdcdb6c4458a', '', 1, 0, '05-38680'),
+('Eric', 'Mendillo', '06-39902@usb.ve', '2d42b211e479cf44a08600924d17cf73', '', 1, 0, '06-39902'),
+('Anselmo', 'Munoz', '07-41260@usb.ve', 'b61a27ee1b107f3afa064ff051799858', '', 1, 0, '07-41260');
 
