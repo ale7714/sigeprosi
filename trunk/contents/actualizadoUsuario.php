16bb22da0659d11a9960a3f8a4bb22d58619b35e
fernandosahmkow1@hotmail.com <fernandosahmkow1@hotmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-14 00:34:49 +0000

    planillas edicion y consulta de usuarios.

---
+++ trunk/contents/actualizadoUsuario.php
@@ -0,0 +1,25 @@
+<div id="main_column">
+
+    <div class="section_w700">
+
+        <font color="white"><h2>Se modificó satisfactoriamente al usuario.</h2>
+        </font color="white">
+        <div class="button_01"><a href="?content=usuarios">Volver a usuarios</a></div>
+    </div>        
+
+    <div class="margin_bottom_20"></div>
+
+    <div class="cleaner"></div>
+</div> <!-- end of main column -->
+
+<!-- end of side column 1 -->
+
+<div class="side_column_w200">
+
+    <!-- barra lateral -->
+    <?php include "sidebars/barraExito.php";?>
+    
+
+</div> <!-- end of right side column -->
+
+<div class="cleaner"></div>
---
+++ trunk/contents/consultaUsuario.php
@@ -0,0 +1,173 @@
+<?php
+if (isset($_SESSION["admin"]) && $_SESSION["admin"] ) {
+?>
+<?php 
+$root = $_SERVER['DOCUMENT_ROOT']."/sigeprosi/";
+include_once $root."class/class.fachadainterfaz.php";
+include_once $root."class/class.Usuario.php";
+if (isset($_GET['email'])) {
+	$fachada = fachadaInterfaz::getInstance();
+	$user = $fachada->consultarUsuario($_GET['email']);
+}
+
+ 
+?>
+
+<div id="main_column">
+
+    <div class="section_w700">
+
+        <h2>Usuario:  <?php echo $user->get('correoUSB')?></h2>
+
+    </div>        
+    <div class="margin_bottom_20"></div>
+
+    <div class="section_w700">
+
+        <table border="0">
+            <tr>
+                <td align="left" width=35.5%>
+                    <LABEL for="nombre"><b>Nombre:</b></LABEL>
+                </td>
+                <td width=64.5%>
+                    <LABEL for="nombre2"><?php echo $user->get('nombre'); ?></LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <LABEL for="apellido"><b>Apellido:</b></LABEL>
+                </td>
+                <td align="left">
+                    <LABEL for="nombre2"><?php echo $user->get('apellido'); ?></LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <LABEL for="correoOpt"><b>Correo Opcional:</b></LABEL>
+                </td>
+                <td align="left">
+                    <LABEL for="correoOpt2">
+                        <?php 
+                            if ($user->get('correoOpcional') == "")
+                                echo "N/A"; 
+                            else
+                                echo $user->get('correoOpcional'); 
+                        ?>
+                    </LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <LABEL for="tel"><b>Telefono:</b></LABEL>
+                </td>
+                <td align="left">
+                    <LABEL for="tel2">
+                        <?php 
+                            if ($user->get('telefono') == "")
+                                echo "N/A"; 
+                            else
+                                echo $user->get('telefono'); 
+                        ?>
+                    </LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <LABEL for="cedula"><b>Cedula o Carnet:</b></LABEL>
+                </td>
+                <td align="left">
+                    <LABEL for="cedula2">
+                        <?php 
+                            if ($user->get('carnetOCedula') == "")
+                                echo "N/A"; 
+                            else
+                                echo $user->get('carnetOCedula'); 
+                        ?>
+                    </LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <LABEL for="rol"><b>Rol:</b></LABEL>
+                </td>
+                <td align="left">
+                    <LABEL for="rol2">
+                        <?php 
+                            switch ($user->get('rol')) {
+                                case 0:
+                                    echo "Administrador.";
+                                    break;
+                                case 1:
+                                    echo "Profesor.";
+                                    break;
+                                case 2:
+                                    echo "Cliente.";
+                                    break;
+                                case 3:
+                                    echo "Estudiante.";
+                                    break;
+                                default:
+                                    echo "Desconocido.";
+                                    break;
+                            }
+                        ?>
+                    </LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">
+                    <LABEL for="estado"><b>Estado:</b></LABEL>
+                </td>
+                <td align="left">
+                    <LABEL for="estado2">
+                        <?php 
+                            switch ($user->get('activo')) {
+                                case 1:
+                                    echo "Activo.";
+                                    break;
+                                default:
+                                    echo "Inactivo.";
+                                    break;
+                            }
+                        ?>
+                    </LABEL>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="submitEditar" value="true"/></td>
+                <td colspan="2">
+                    <div class="button_01"><a href="?content=usuarios">Volver</a></div>
+                <?php
+                    echo '<div class="button_01"><a href="?content=editaUsuario&email='.$user->get('correoUSB').'">Editar</a></div>';
+                ?>
+                </td>
+            </tr>
+			
+
+        </table>
+
+        <h3> </h3>
+
+
+    </div>  
+
+    <div class="margin_bottom_20"></div>
+    <div class="cleaner"></div>
+</div> <!-- end of main column -->
+
+<!-- end of side column 1 -->
+
+<div class="side_column_w200">
+
+    <!-- barra lateral -->
+
+</div> <!-- end of right side column -->
+
+<div class="cleaner"></div>
+<?php
+} else {
+?>
+
+<?php
+}
+?>
