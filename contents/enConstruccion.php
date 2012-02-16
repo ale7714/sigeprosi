408b36e0a343440e2ca56bf872ff91db206e84e6
joseanmun@gmail.com <joseanmun@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-16 14:30:46 +0000

    agregada vista de Enconstruccion para los casos de uso que aun no se han implementado.

---
+++ contents/enConstruccion.php
@@ -0,0 +1,25 @@
+<? //if (!isset ($_POST['acepto'])) header('Location:principal.php?content=previoSolicitud')?>
+<div id="main_column">
+    <div class="section_w700"><h2>Temporalmente no disponible.</h2></div>        
+    <div class="section_w702">
+        <table align="center"><tr><td>
+			<table id="etapasGrid"><tr><td/></tr></table> 
+			<div id="etapasPager"></div> <p></p></td></tr>
+		</table>
+		<div align="center"><IMG src="images/ICO/Under-construction.ico" width="200" height="200"></div>
+		<center><b><span class="em_text"><font size=2 >Seccion en proceso de constuccion.</font></span></b></center>
+		<div class="margin_bottom_20"></div>
+		<div class="cleaner"></div>
+	</div>
+</div> <!-- end of main column -->
+
+<!-- end of side column 1 -->
+
+<div class="side_column_w200">
+
+    <!-- barra lateral -->
+
+</div> <!-- end of right side column -->
+
+<div class="cleaner"></div>
+
