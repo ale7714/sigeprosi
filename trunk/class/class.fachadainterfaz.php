2a6e3136bd316bae1279b5fd9a389174cd2d5932
duque.cristian@gmail.com <duque.cristian@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-02-24 06:22:33 +0000

    alguien esta usando rutas absolutas para los archivos recuerden que los usuarios del sistema no tendran acceso a carpetas del sistema operativo, valga la redundancia

--- trunk/class/class.fachadainterfaz.php
+++ trunk/class/class.fachadainterfaz.php
@@ -6,7 +6,10 @@
 		NOMBRE DEL ARCHIVO:	CLASS.FACHADAINTERFAZ.PHP
 		DESCRIPCION:
 	*/
-$root = $_SERVER['DOCUMENT_ROOT']."/sigeprosi/";
+if ($_SERVER["SERVER_ADDR"]=="159.90.168.83" || $_SERVER["SERVER_ADDR"]!="127.0.0.1" )
+  $root = "/home/ps6116-02/public_html/Sigeprosi/";
+else
+  $root = $_SERVER['DOCUMENT_ROOT']."/sigeprosi/";
 include_once $root."/snippets/generarSal.php";
 include_once "class.Encrypter.php";
 include_once "class.Usuario.php";
