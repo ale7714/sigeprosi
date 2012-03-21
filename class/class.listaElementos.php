75685c46324bf7537185912925abba1969576b87
duque.cristian@gmail.com <duque.cristian@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-03-21 05:04:52 +0000

    agregado gestionar catalogo, editar elemento, falta mejorar este y hacer eliminar y crear elemento

---
+++ class/class.listaElementos.php
@@ -0,0 +1,105 @@
+<?php 
+	/*	UNIVERSIDAD SIMON BOLIVAR
+		PERIODO:			ENE-MAR 2012
+		MATERIA: 			SISTEMAS DE INFORMACION II
+		NOMBRE DEL ARCHIVO:	class.listaProyecto.php
+	*/
+include_once "fBaseDeDatos.php";
+include_once "class.Elementos.php";
+
+class listaElementos extends elementos {
+
+		/*	Parametros de entrada:
+					NINGUNO
+		Parametros de salida: 
+					Objeto del tipo listaSolicitud
+		Descripcion	: Constructor de la clase listaSolicitud			
+		*/
+        function __construct() {
+        }
+		
+		/*	Parametros de entrada:
+					$n	: 	variable del tipo int que representa el num
+		Parametros de salida: 
+					cliente:	Instancia del tipo solicitud si la busqueda fue exitosa
+					NULL: 		De lo contrario
+		Descripcion	: 
+					Funcion que	realiza la busqueda de una solicitud segun un parametro
+		*/		
+		public function listar(){
+			$fachaBD= fBaseDeDatos::getInstance();
+			$nombre = array ();
+			$nombre[0] = "elementos";
+			$columnas = array();
+			$columnas[0]= "*";
+			$parametros= array ();
+			$parametros[0] = "";
+			$valores= array();
+			$valores[0]= "";
+			$Busqueda= new BusquedaSimple($nombre,$columnas,null);					
+			$result = $fachaBD->search($Busqueda);
+			$listarray = array();
+			$i=0;
+			
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {	
+				//print "Hola";
+				$etapa = new elementos($row['nombre']);
+				$etapa->set($row['id']);
+				$listarray[$i] = $etapa;
+				$i++;
+			}
+			
+			return $listarray;		
+		}
+		
+		public function filtrar($n,$p){
+			$fachaBD= fBaseDeDatos::getInstance();
+			$nombre = array ();
+			$nombre[0] = "elemento";
+			$columnas = array();
+			$columnas[0]= "*";
+			$parametros= array ();
+			$parametros[0] = $p;
+			$valores= array();
+			$valores[0]= $n;
+			$Busqueda= new BusquedaSimple($nombre,$columnas,null);					
+			$result = $fachaBD->search($Busqueda);
+			$listarray = array();
+			$i=0;
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {	
+				//print "Hola";
+				$actividad1 = new elementos($row['nombre']);
+				$listarray[$i] = $actividad1;
+				$i++;
+			}
+			return $listarray;		
+		}
+        
+        public function cargar($catalog,$sigid,$sigord,$start,$limit) {
+			$fachaBD= fBaseDeDatos::getInstance();
+			$nombre = array ();
+			$nombre[0] = "elemento";
+      $columnas = array();
+			$columnas[0]= "*";
+			$parametros= array ();
+			$parametros[0] = "nombreCatalogo";
+			$valores= array();
+			$valores[0]= $catalog;
+      $ord = array();
+      $ord[0] = $sigord;
+      $join = array();
+      $join[0] = false;
+			$Busqueda= new BusquedaCompleta($nombre,$columnas,$parametros,$valores,"=","",
+                                            $ord,$sigid,$start,$limit,$join);
+			$c= $fachaBD->search($Busqueda);
+			$listarray = array();
+			$i=0;
+			while($lista=mysql_fetch_array($c,MYSQL_ASSOC)) {
+				$listarray[$i]=$lista;
+				$i=$i+1;
+			}
+			return $listarray;		
+		}
+		
+}
+?>
---
+++ contents/gestionarCatalogo.php
@@ -0,0 +1,96 @@
+<link rel="stylesheet" type="text/css" media="screen" href="estilos/custom-theme/jquery-ui-1.8.17.custom.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="estilos/ui.jqgrid.css" />
+
+<style type="text/css">
+html, body {
+    margin: 0;
+    padding: 0;
+    font-size: 86.6%;
+}
+</style>
+
+<script src="jscripts/js/jquery-1.5.2.min.js" type="text/javascript"></script>
+<script src="jscripts/js/i18n/grid.locale-en.js" type="text/javascript"></script>
+<script src="jscripts/js/jquery.jqGrid.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+$(function(){ 
+  $("#catalogosGrid").jqGrid({
+    url:'acciones/cargarCatalogos.php',
+    datatype: 'xml',
+    mtype: 'GET',
+    colNames:['Nombre'],
+    colModel :[ 
+      {name:'nombre', index:'nombre', width:200}, 
+    ],
+    pager: '#catologosPager',
+    toolbar:[true,"top"],
+    height: 'auto',
+    rowNum:20,
+    rowList:[20,40,60],
+    sortname: 'invid',
+    sortorder: 'asc',
+    viewrecords: true,
+    gridview: true,
+    ondblClickRow: function(id){
+      var val = jQuery(this).getRowData(id);
+			var botonoes = document.getElementsByName("group1");
+			var i;
+			for(i=0;i<botonoes.length;i++) if (botonoes[i].checked) window.location = "?content="+botonoes[i].value+"&nombre="+val['nombre'];
+    },
+    caption: 'Catálogos',
+  }).navGrid('#pager1',{
+     edit: false,
+     add: false,
+     del: false
+ }); 
+}); 
+</script>     
+<div id="main_column">
+   <div class="section_w701"><font size="6" face="arial"><b>Gestionar Catalogo:</b></font>  </div>  
+
+    <div class="section_w702">
+        
+        <table align="center"><tr><td>
+			<table id="catalogosGrid"><tr><td/></tr></table> 
+			<div id="catalogosPager"></div> <p></p></td></tr>
+		</table>
+		<?php if (((isset($_SESSION['profesor'])) && ($_SESSION['profesor']))){?>
+		<center><b> 
+        <span class="em_text"><font size=2 >Presione doble click sobre el cat&aacute;logo a consultar.</font></span>
+				<!--span class="em_text"><font size=2 >&iquest;Qu&eacute; desea realizar sobre cat&aacute;		logos?</font></span-->
+        </b></center>
+		<div align="center"><font size=2 >
+                <!--input type="radio" name="group1" value="consultaCatalogo" checked > Consultar -->
+                <input type="radio" name="group1" value="listarElementos" checked> Listar elementos
+				</font>
+        </div>
+		<?php } ?>
+    </div> 
+<?php 
+if (((isset($_SESSION['profesor'])) && ($_SESSION['profesor']))){?>
+	<div class="section_w700">
+		<center>
+		<IMG SRC="images/ICO/add.png" class="pointer" onclick='javascript: alert("esta funcion no esta implementada aun");' width="50" height="50" type="button" title="Crear Nuevo Catalogo" onMouseOver="javascript:this.width=60;this.height=60"  onMouseOut="javascript:this.width=50;this.height=50"> 
+		</center>
+    </div>  
+<?php } ?>
+</div> <!-- end of main column -->
+	
+<!-- end of side column 1 -->
+
+<div class="side_column_w200">
+    <?php
+    if (isset($_SESSION['admin'])){
+        include 'sidebars/barraEnSesion.php';
+        include 'sidebars/barraEnlaces.php';
+    }else{
+        include 'sidebars/barraInicioSesion.php';
+        include 'sidebars/barraEnlaces.php';
+    }
+    ?>
+    <!-- barra lateral -->
+
+</div> <!-- end of right side column -->
+
+<div class="cleaner"></div>
