de3bdd1239f21bb55861b72c410f614574d5c9a5
aept7714@gmail.com <aept7714@gmail.com@133b273c-1533-530f-d031-d9a6d46bf633>
2012-07-06 05:41:25 +0000

    Evaluacion para estudiantes

--- trunk/menu/menuEstudianteCoord.php
+++ trunk/menu/menuEstudianteCoord.php
@@ -3,8 +3,8 @@
 	<div id="templatemo_menu">
     	<ul>
             <li><a href="?content=inicio" <?php if ($page=='inicio'){?>class="current fast"<?php }?>><b>Inicio</b></a></li>
-            <li><a href="?content=gestionarProyecto" <?php if ($page=='proyectos'){?>class="current fast"<?php }?>><b>Proyectos</b></a></li>
-            <li><a href="?content=enConstruccion" <?php if ($page=='evaluaciones'){?>class="current fast"<?php }?>><b>Evaluaciones</b></a></li>
+            <li><a href="?content=gestionarProyecto" <?php if ($page=='consultaProyecto'){?>class="current fast"<?php }?>><b>Proyectos</b></a></li>
+            <li><a href="?content=gestionarEvaluacion" <?php if ($page=='gestionarEvaluacion' || $page=='consultoEvaluacion' ){?>class="current fast"<?php }?>><b>Evaluaciones</b></a></li>
 			<li><a href="?content=gestionarCasodeuso" <?php if ($page=='gestionarCasodeuso' || $page=='registroCasoUso' || $page=='editaCasoUso'){?>class="current fast"<?php }?>><b>Casos de Uso</b></a></li>
             <li><a href="?content=gestionarIteracion" <?php if ($page=='gestionarIteracion'){?>class="current fast"<?php }?>><b>Iteraciones</b></a></li>
 						<li><a href="?content=gestionarDocumentos" <?php if ($page=='gestionarDocumentos'){?>class="current fast"<?php }?>><b>Documentos</b></a></li>
